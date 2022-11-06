10295d080805f7a156243f25b369703a42b5379e
takumimzd <retnuh.takumi@gmail.com>
2022-11-06 14:55:12 +0900

    demo-react-content-loader  Please enter the commit message for your changes. Lines starting

---
+++ pages/components/MyLoader.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { Instagram, Facebook } from "react-content-loader";
+
+export const MyInstagramLoader = () => <Instagram />;
+export const MyFacebookLoader = () => <Facebook />;
